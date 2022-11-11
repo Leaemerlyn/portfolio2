842ca7c42ea58c6a6995d0928f4659fb05e96518
Leaemerlyn <leaodovia@gmail.com>
2022-11-11 07:54:23 -0500

    project card :)

--- src/ProjectCardData.js
+++ src/ProjectCardData.js
@@ -1,6 +1,6 @@
 export let ProjectCardData = {
     Spotify : {
-        description: "This is the description",
+        description: "This is the description that is really long hehehe hi hi hi i love bread",
         pills: ['API' , 'React', 'WebDev'],
         coverImage: require('./Rectangle 29.jpeg')
     }
